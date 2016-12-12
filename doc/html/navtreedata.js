84de531b2b8322e70f09023ec9754867bad81cc5
rnwatanabe <renato.navillewatanabe@gmail.com>
2016-12-12 13:35:32 -0200

    documentation

--- doc/html/navtreedata.js
+++ doc/html/navtreedata.js
@@ -1,34 +1,11 @@
 var NAVTREE =
 [
-  [ "PyReMoto", "index.html", [
-    [ "ReMoto in Python", "index.html", null ],
-    [ "Packages", null, [
-      [ "Packages", "namespaces.html", "namespaces" ],
-      [ "Package Functions", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ]
-    ] ]
-  ] ]
+  [ "PyReMoto", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"_axon_delay_8py.html",
-"namespace_pulse_conductance_state.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,33 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghiklmnoprstuvy",
-  1: "acmnops",
-  2: "acmnps",
-  3: "acmnps",
-  4: "_acdgilprst",
-  5: "abcdefghiklmnprstuvy",
-  6: "r"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Pages"
 };
 
